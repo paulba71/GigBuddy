733dfd5e82f72392976193cd27c4d82ca45f9274
Paul Barnes <paulba71@hotmail.com>
2025-04-23 08:29:53 +0100

    Initial Commit

---
+++ GigBuddy/GigBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigBuddyApp.swift
+//  GigBuddy
+//
+//  Created by Paul Barnes on 23/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct GigBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
